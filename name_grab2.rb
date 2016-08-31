0ab5c2e5abce85ccf6ab9da569179d64c45edfff
Amy Carney <amy.carney@alaska.gov>
2016-08-31 00:33:08 +0000

    Learning app development with Ruby. Get facts from user and echo back.

---
+++ name_grab2.rb
@@ -0,0 +1,7 @@
+puts 'Hello there, and what\'s your name?'
+name = gets.capitalize
+puts 'Your name is ' + name + '?  What a lovely name!'
+puts 'How old are you, ' + name + '?'
+age = gets
+puts 'Pleased to meet you, ' + name + '. ' + age + 'years old is a good age to be.'
+puts "You\'re rocking it!"
